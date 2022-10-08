3ce970168dbf1685ba107eb5b4a2f2bccc3d9195
Jijo Derick Abraham <69760206+jijoderick@users.noreply.github.com>
2022-10-08 18:57:14 +1000

    Create broadcast.py

---
+++ broadcast.py
@@ -0,0 +1,18 @@
+from mpi4py import MPI
+import numpy
+sendbuf = []
+comm = MPI.COMM_WORLD
+if comm.rank == 0:
+    m= numpy.random.randn(comm.size, comm.size)
+    print("Original array on rank 0:\n" + str(m))
+    sendbuf = m
+v= comm.scatter(sendbuf, root = 0)
+print("I got this array:\n" + str(v))
+v = v*v
+recvbuf = comm.gather(v, root = 0)
+if comm.rank == 0:
+    print("New array on rank 0:\n"+ str(numpy.array(recvbuf)))
+if MPI.COMM_WORLD.rank == 0:
+    sendbuf = "Done!"
+recvbuf =MPI.COMM_WORLD.bcast(sendbuf, root = 0)
+print(recvbuf)
