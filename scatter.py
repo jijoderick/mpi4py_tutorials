b777b505126c6a20e144bf30a9c632546d6914ff
Jijo Derick Abraham <69760206+jijoderick@users.noreply.github.com>
2022-10-08 18:58:16 +1000

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,10 @@
+from mpi4py import MPI
+import numpy
+sendbuf = []
+comm = MPI.COMM_WORLD
+if comm.rank == 0:
+    m= numpy.random.randn(comm.size, comm.size)
+    print("Original array on rank 0:\n" + str(m))
+    sendbuf = m
+v= comm.scatter(sendbuf, root = 0)
+print("I got this array:\n" + str(v))
