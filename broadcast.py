e7d88712a1cb71c20991f9ec7f5c07fbf75867ad
jijoderick <derickjijo@gmail.com>
2022-10-09 12:37:41 +1000

    edited the broadcast tutorial

--- broadcast.py
+++ broadcast.py
@@ -7,12 +7,13 @@ if comm.rank == 0:
     print("Original array on rank 0:\n" + str(m))
     sendbuf = m
 v= comm.scatter(sendbuf, root = 0)
-print("I got this array:\n" + str(v))
+print("Rank "+str(comm.rank)+"I got this array:\n" + str(v))
 v = v*v
 recvbuf = comm.gather(v, root = 0)
 if comm.rank == 0:
     print("New array on rank 0:\n"+ str(numpy.array(recvbuf)))
 if MPI.COMM_WORLD.rank == 0:
     sendbuf = "Done!"
-recvbuf =MPI.COMM_WORLD.bcast(sendbuf, root = 0)
-print(recvbuf)
+recv =MPI.COMM_WORLD.bcast(sendbuf, root = 0)
+recv2= MPI.COMM_WORLD.bcast(recvbuf, root = 0)
+print("My rank is "+str(comm.rank)+",From root,  I received the message "+recv+"\n the following array " +str(recv2))
