2ae091cc610258499436ff77652a87375374fd4d
jijoderick <derickjijo@gmail.com>
2023-01-17 20:20:44 +1000

    added comment in scatter.py

--- scatter.py
+++ scatter.py
@@ -1,7 +1,8 @@
+# code for scatter communication
 from mpi4py import MPI
 import numpy
 sendbuf = []
-comm = MPI.COMM_WORLD
+comm = MPI.COMM_WORLD # communicator
 if comm.rank == 0:
     m= numpy.random.randn(comm.size, comm.size)
     print("Original array on rank 0:\n" + str(m))
